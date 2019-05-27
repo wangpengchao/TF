b262e827bc55bfd5dc0ffcf2fe9739dd488a336c
WangPChao <wangpengchao__just_thinking@outlook.com>
2019-05-27 17:02:24 +0800

    2019年5月27日17:01:29 修改mnist_train_acc1.py文件：     添加注释，理清代码的结构和含义

--- mnist_train_acc1.py
+++ mnist_train_acc1.py
@@ -1,69 +1,39 @@
 import tensorflow as tf
-import numpy
-import matplotlib.pyplot as plt
 from tensorflow.examples.tutorials.mnist import input_data
 
+# 指定mnist数据集的位置，
 mnist = input_data.read_data_sets('data/', one_hot=True)
-# print('type of mnist is %s' % (type(mnist)))
-# print('number of train data is %d ' % (mnist.train.num_examples))
-# print('number of test data is %d ' % (mnist.test.num_examples))
 
 trainImg = mnist.train.images
 trainLabel = mnist.train.labels
 testImg = mnist.test.images
 testLabel = mnist.test.labels
-
 print('MNIST loaded')
 
-# print('type of trainImg is %s' % type(trainImg))
-# print('type of trainLabel is %s' % type(trainLabel))
-# print('type of testImg is %s' % type(testImg))
-# print('type of testLabel is %s' % type(testLabel))
-#
-print('shape of trainImg is %s' % (trainImg.shape,))
-print('shape of trainLabel is %s' % (trainLabel.shape,))
-print('shape of testImg is %s' % (testImg.shape,))
-print('shape of testLabel is %s' % (testLabel.shape,))
+x = tf.placeholder(dtype=tf.float32, shape=[None, 784])  # ?*784 大小的二维矩阵
+y = tf.placeholder(dtype=tf.float32, shape=[None, 10])  # ?*10 大小的二维矩阵
+W = tf.Variable(tf.zeros(shape=[784, 10]))  # W参数为 784*10 大小的二维矩阵
+b = tf.Variable(tf.zeros(shape=[10]))  # b参数为 长度为 10 的向量
 
-print('#######################')
-print()
-nsample = 5
-randidx = numpy.random.randint(trainImg.shape[0], size=nsample)  # 返回一个包含5个随机选取的样例行下标的列表
-# print('shape[0]: ', trainImg.shape[0])
-print('randidx: ', randidx)
-# print(trainLabel[0])
+actv = tf.nn.softmax(tf.matmul(x, W) + b)  # 预测结果: Tensor("Softmax:0", shape=(?, 10), dtype=float32)
+cost = tf.reduce_mean(-tf.reduce_sum(y * tf.log(actv), reduction_indices=1))  # y*log(actv)获取真正值得交叉熵损失
 
+# print('y.shape: ', y.shape)  # y.shape:  (?, 10)
+# print('actv.shape: ', actv.shape)  # actv.shape:  (?, 10)
 
-# 显示随机选取的5个样本
-# for i in randidx:
-#     curr_img = numpy.reshape(trainImg[i, :], (28, 28))
-#     curr_label = numpy.argmax(trainLabel[i, :])
-#     print('curr_label: ', curr_label)
-#     print('trainLabel[%d]: ' % (i), trainLabel[i])
-#     print('' + str(i) + 'the training date label is ' + str(curr_label))
-#
-#     plt.matshow(curr_img, cmap=plt.get_cmap('gray'))
-#     plt.title('' + str(i) + 'the training data label is ' + str(curr_label))
-#     plt.show()
+learn_rate = 0.01
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learn_rate).minimize(cost)  # 梯度下降求解的优化器
 
-x = tf.placeholder(dtype=tf.float32, shape=[None, 784])
-y = tf.placeholder(dtype=tf.float32, shape=[None, 10])
-W = tf.Variable(tf.zeros(shape=[784, 10]))
-b = tf.Variable(tf.zeros(shape=[10]))
+# 1表示按行获取最大值得下标，查看查看预测结果actv返回的下标和真实的y的下标是否一致
+prediction = tf.equal(tf.argmax(actv, 1), tf.argmax(y, 1))
 
-actv = tf.nn.softmax(tf.matmul(x, W) + b)
-cost = tf.reduce_mean(-tf.reduce_sum(y * tf.log(actv), reduction_indices=1))
-
-learing_rate = 0.01
-optm = tf.train.GradientDescentOptimizer(learning_rate=learing_rate).minimize(cost)
-
-prediction = tf.equal(tf.argmax(actv, 1), tf.argmax(y, 1))  # 1表示行，查看actv返回的下标和y返回的下标是否一致
-accr = tf.reduce_mean(tf.cast(prediction, 'float'))  # 计算精确度
+accuracy = tf.reduce_mean(tf.cast(prediction, 'float'))  # 使用均值衡量准确率，将true和false转换为float类型（true-》1.0， false-》0.0）
 init = tf.global_variables_initializer()
 
 # sess = tf.InteractiveSession()
 train_epochs = 50  # 训练轮数
-batch_size = 100  # 每次迭代选择的样本数
+batch_size = 100  # 每100个为一个batch
+batchs_amount = int(mnist.train.num_examples/batch_size)  # 表示每轮训练中有 batchs_amount 个 batch
 display_step = 5  # 每5轮显示一次
 
 sess = tf.Session()
@@ -71,20 +41,25 @@ sess.run(init)
 
 for epoch in range(train_epochs):
     avg_cost = 0.0
-    num_batch = int(mnist.train.num_examples/batch_size)
-
-    for i in range(num_batch):
+    for i in range(batchs_amount):
         batch_xs, batch_ys = mnist.train.next_batch(batch_size)
         feeds = {x: batch_xs, y: batch_ys}
-        sess.run(optm, feed_dict=feeds)
-        avg_cost += sess.run(cost, feed_dict=feeds)/num_batch
+        sess.run(optimizer, feed_dict=feeds)
+        avg_cost += sess.run(cost, feed_dict=feeds)/batchs_amount
 
-    #display
+    # print('batch_xs: ', batch_xs)
+    # print('batch_ys: ', batch_ys)
+    # print()
+
+    #每5轮显示一次
     if epoch%display_step == 0:
+
         feeds_train = {x: batch_xs, y: batch_ys}
         feeds_test = {x: mnist.test.images, y: mnist.test.labels}
-        train_acc = sess.run(accr, feed_dict=feeds_train)
-        test_acc = sess.run(accr, feed_dict=feeds_test)
+
+        train_acc = sess.run(accuracy, feed_dict=feeds_train)
+        test_acc = sess.run(accuracy, feed_dict=feeds_test)
+
         print('Epoch: %03d/%03d cost: %.9f train_acc: %.3f test_acc: %.3f'
               % (epoch, train_epochs, avg_cost, train_acc, test_acc))
 
